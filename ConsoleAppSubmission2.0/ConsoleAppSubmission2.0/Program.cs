6c55d9cc2edf6be24c8e0a6ed56cc59968b24df4
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2024-02-16 10:25:44 -0700

    Created and Submitted

---
+++ ConsoleAppSubmission2.0/ConsoleAppSubmission2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppsubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Do a boolean comparison using a while statement.
+            //Do a boolean comparison using a do while statement.
+            //Add comments 
+             int i = 0;
+            do {
+                Console.WriteLine("i = " + i);
+                i++;
+            } while (i < 5);
+
+            int j = 0;
+            while (j < 10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+        }
+    }
+}
