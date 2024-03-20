7ca5a91d37b5b19e03339d09d3d5957a4bde3990
Mstewart93 <121573726+Mstewart93@users.noreply.github.com>
2024-03-20 05:25:12 -0600

    Fix
    Working through all the bugs and fixing them with knowledge, as well as research to fill areas I am still unsure of.

--- 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/Program.cs
+++ 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/Program.cs
@@ -46,73 +46,78 @@ namespace Arraysubmission
                 
             
 
-                //Assignment part 3
-                
-                //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
-                //Add another loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
-                for (int j = 0; j < 10; j++)
-                    {
-                    Console.WriteLine("Hello, this should post 9 times :)");
-                    }       
-                while (int k = 1; k <= 10; k++)
-                                {Console.WriteLine("This should post 10 times however");}
-
-                            //Assignment Part 4
-                            
+            //Assignment part 3
             
-                                //A list of strings where each item in the list is unique.
-                                string[] str2 = {'mouse','horse','leopard','zebra','orangatang'}
-                                
-                                {Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
-                                string[] userinput = Console.ReadLine().ToLower;
-                                
-                                for (int m = 0; m>str2.Length; m++)
-                                    {
-                                        if (str2[m] == userinput)
-                                        {Console.WriteLine("Your Favorite Animal is on the list!" + userinput.IndexOf(userinput));
-                                        }
-                                    
-                                        else
-                                        {
-                                    Console.WriteLine( "Your Animal is not on the list");
-                                        }
-                                    }
-                                
-                                
+            //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
+            //Add another loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
+            
+            for (int j = 0; j < 10; j++)
+                {
+                Console.WriteLine("Hello, this should post 9 times :)");
+                }  
+            int k = 1;     
+            while (k <= 10 )
+                            {Console.WriteLine("This should post 10 times however");}
+                        k++;
+           
+           
+            //Assignment Part 4 
+            //A list of strings where each item in the list is unique.
+            string []animalStr;
+            animalStr = ["mouse","horse","leopard","zebra","orangatang"];
+            
+            {Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
+            string userinput = Console.ReadLine();
+            string lowerUser = userinput.ToLower();
+            
+            for (int m = 0; m>animalStr.Length; m++)
+                {
+                    if (animalStr[m] == userinput)
+                    {Console.WriteLine("Your Favorite Animal is on the list!" + userinput.IndexOf(userinput));
+                    }
+                
+                    else
+                    {
+                Console.WriteLine( "Your Animal is not on the list");
+                    }
+                }
+                
+                
 
                         
 
-                            //Assignment Part 5
+            //Assignment Part 5
 
-                        string[] color = ["red","orange","yellow","green","blue","purple","black","brown","gray","grey", "yellow"];
-                            Console.WriteLine("Please enter your favorite color bellow:  ");
-                                string usercolor = Console.ReadLine();
-                                for (int n = 0; n < str1.Length; n++)
-                    {
-                                    if (color  == usercolor);
-                                    Console.WriteLine("Your Color is on the list" +usercolor.IndexOf(usercolor));
-                                    else 
-                                    Console.WriteLine("Your color is not on the lsit")
-                                    }
-                                                
+            string[] color = ["red","orange","yellow","green","blue","purple","black","brown","gray","grey", "yellow"];
+                Console.WriteLine("Please enter your favorite color bellow:  ");
+                    string usercolor = Console.ReadLine();
+                    for (int n = 0; n < str1.Length; n++)
+                        {
+                        if (color  = usercolor);
+                        Console.WriteLine("Your Color is on the list" +usercolor.IndexOf(usercolor))
+                        
+                        else 
+                        Console.WriteLine("Your color is not on the lsit")
+                        };
+                                    
 
-                                            //Assignment Part 6
-                                            foreach (string z in color)
+                                //Assignment Part 6
+                                foreach (string z in color)
 
-                                            {
-                                                if (color.Equals(color[]) = true)
-                                                
-                                                {Console.WriteLine(color[z] + "This item is not Unique");}
+                                {
+                                    if (color.Equals(color[]) = true)
+                                    
+                                    {Console.WriteLine(color[z] + "This item is not Unique");}
 
-                                                else
-                                                Console.WriteLine(color[z]+ "This item is Unique")
-                                            }
+                                    else
+                                    Console.WriteLine(color[z]+ "This item is Unique")
+                                }
 
-                                            Console.ReadLine();
-                                            }
-                                        }
-                                    }
-                                    }
+                                Console.ReadLine();
+            }
+        }
+    }
+ }
 
 
 //Unfortunately, I'm not able to get this to run. Many errors are preventing this from running, as I'm seeing 36 errors in the Error List. The bulk of these look like they're either syntax errors or typos when referencing variables. For example, on line 105 you have the following:
