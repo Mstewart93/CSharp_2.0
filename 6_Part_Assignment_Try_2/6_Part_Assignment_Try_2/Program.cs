52d0c16778fa2f86e18d575d70bf7d061312618e
Mstewart93 <melyssaannestewart@gmail.com>
2024-09-13 15:31:01 -0700

    Update to Part 6
    Using provided resources I updated part 6 to try and debug.

--- 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/Program.cs
+++ 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/Program.cs
@@ -21,13 +21,13 @@ namespace Arraysubmission
             Console.WriteLine("Please Enter your favorite animal bellow:  ");
             string useranimal = Console.ReadLine();
             for (int i = 0; i < animalList.Count; i++)
-                { Console.WriteLine(animalList[i]);};
+            { Console.WriteLine(animalList[i]); };
 
 
 
 
             //Assignment Part 2 
-                
+
             //x = 1;
             //while (x == 1);
             //Console.WriteLine("What is your name?");
@@ -39,91 +39,90 @@ namespace Arraysubmission
             while (x > 0)
             {
                 Console.WriteLine("Please Enter Your name ");
-                Console.ReadLine(); 
+                Console.ReadLine();
                 Console.WriteLine("Welcome!");
                 x--;
             }
-                
-            
+
+
 
             //Assignment part 3
-            
+
             //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
             //Add another loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
-            
+
             for (int j = 0; j < 10; j++)
-                {
+            {
                 Console.WriteLine("Hello, this should post 9 times :)");
-                }  
-            int k = 1;     
-            while (k <= 10 )
-              {Console.WriteLine("This should post 10 times however");
-                            
-                 k++;
-              }
-                       
-           
-           
+            }
+            int k = 1;
+            while (k <= 10)
+            { Console.WriteLine("This should post 10 times however");
+
+                k++;
+            }
+
+
+
             //Assignment Part 4 
             //A list of strings where each item in the list is unique.
-            string []animalStr;
-            animalStr = ["mouse","horse","leopard","zebra","orangatang"];
-            
-            {Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
-            string userinput = Console.ReadLine();
-            string lowerUser = userinput.ToLower();
-            
-            for (int m = 0; m<animalStr.Length; m++)
+            string[] animalStr;
+            animalStr = ["mouse", "horse", "leopard", "zebra", "orangatang"];
+
+            { Console.WriteLine("Let's see if your favorite animal is in the list. Please enter your favorite animal:   ");
+                string userinput = Console.ReadLine();
+                string lowerUser = userinput.ToLower();
+
+                for (int m = 0; m < animalStr.Length; m++)
                 {
                     if (animalStr[m] == userinput)
-                    {Console.WriteLine("Your Favorite Animal is on the list!" + m);
+                    { Console.WriteLine("Your Favorite Animal is on the list!" + m);
                     }
-                
-                    else if(!animalStr.Contains(lowerUser)) 
+
+                    else if (!animalStr.Contains(lowerUser))
                     {
-                        Console.WriteLine( "Your Animal is not on the list");
-                        break; 
+                        Console.WriteLine("Your Animal is not on the list");
+                        break;
                     }
                 }
-                
-                
 
-                        
 
-            //Assignment Part 5
 
-            string[] color;
-            color = ["red","orange","yellow","green","blue","purple","black","brown","gray","grey", "yellow"];
-            Console.WriteLine("Please enter your favorite color bellow:  ");
-            string usercolor = Console.ReadLine();
-            for (int n = 0; n < color.Length; n++)
+
+
+                //Assignment Part 5
+
+                string[] color;
+                color = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "gray", "grey", "yellow"];
+                Console.WriteLine("Please enter your favorite color bellow:  ");
+                string usercolor = Console.ReadLine();
+                for (int n = 0; n < color.Length; n++)
                 {
-                    if (color[n]  == usercolor)
-                    {Console.WriteLine("Your Color is on the list" + n);
+                    if (color[n] == usercolor)
+                    { Console.WriteLine("Your Color is on the list" + n);
                     }
-                    
-                    else if(!color.Contains(usercolor))  
+
+                    else if (!color.Contains(usercolor))
                     {
-                        Console.WriteLine( "Your Color is not on the list");
-                       
+                        Console.WriteLine("Your Color is not on the list");
+
                     }
                 }
-                                    
 
-            //Assignment Part 6
-            List<string> booklist = ["Harry Potter", "Lord of the Rings","Hobbit","Howls Moving Castle", "Harry Potter"];
-            List<string> duplicate = [];
-            foreach (string z in booklist) 
-    
+
+                //Assignment Part 6
+                List<string> booklist = new List<string>()["Harry Potter", "Lord of the Rings", "Hobbit", "Howls Moving Castle", "Harry Potter"];
+                List<string> duplicate = new List<string>()[];
+                foreach (string z in booklist)
+
                 {
                     if (duplicate.Contains(z)) // loop that will compare itehm to eachother 
-                    {Console.WriteLine("This Item is unique");
-                    duplicate.Add(z);}
-                    else 
-                    {Console.WriteLine("This book entry is a duplicate");};
-                }
-
-
+                        Console.WriteLine("This Item is unique");
+                        duplicate.Add(z); }
+                    else
+                    Console.WriteLine("This book entry is a duplicate");
+            }
+        }
             }
         }
     }
--- 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/obj/Release/net8.0/6_Part_Assignment_Try_2.AssemblyInfo.cs
+++ 6_Part_Assignment_Try_2/6_Part_Assignment_Try_2/obj/Release/net8.0/6_Part_Assignment_Try_2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("6_Part_Assignment_Try_2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c942cee814ef6a97b0ec0a031c70342d76960b")]
 [assembly: System.Reflection.AssemblyProductAttribute("6_Part_Assignment_Try_2")]
