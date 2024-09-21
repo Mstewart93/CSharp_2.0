759c78787d2d5a6a7b6ec8f29af8e0fecc68a145
Mstewart93 <melyssaannestewart@gmail.com>
2024-09-21 09:02:21 -0700

    Assignment Creation
    Created the Assignment and followed instructions to make method submissions.

---
+++ Method_Class_Submission/OperationHold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Submission
+{
+    public class OperationHold
+    {
+        public static void UserOperation(int userNumber, int userNumber2)
+        {
+            Console.WriteLine(userNumber + 12);
+            Console.WriteLine(userNumber2);
+        }
+    }
+
+}
+
---
+++ Method_Class_Submission/Program.cs
@@ -0,0 +1,32 @@
+﻿using Method_Class_Submission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calling_Method_Submission
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number"); //request user input
+            string numberInput = Console.ReadLine(); //collect user input
+            int userNumber = Convert.ToInt32(numberInput); // convert user input into an integer
+            Console.WriteLine("Please enter a second number");
+            string numberInput2 = Console.ReadLine();
+            int userNumber2 = Convert.ToInt32(numberInput2);
+
+            OperationHold operationHold = new OperationHold();
+            OperationHold.UserOperation(userNumber,userNumber2);
+
+        }
+
+    }
+    }
+
+
+
