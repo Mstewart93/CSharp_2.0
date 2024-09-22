4018bfb9fb9f51ac608714c1e7235f0cde028dd2
Mstewart93 <melyssaannestewart@gmail.com>
2024-09-22 10:07:07 -0600

    Assignment
    created the assignment according to instruction, and then submitted.

---
+++ Class_Submission_Assignment/DivisionOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    public  class DivisionOperation
+    {
+
+
+        public static void userDivision(int userNumber)
+        {
+
+            Console.WriteLine(userNumber / 2);
+        }
+
+        public static void userDivision(out int userNumber)
+        {
+            userNumber = 0;  
+            
+        }
+    }
+}
---
+++ Class_Submission_Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿//Perform these actions and create a console app that includes the following:
+
+//Create a method with output parameters.
+
+//Overload a method.
+
+//Declare a class to be static., static and void is what I have been doing the entire tiem 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace Class_Submission_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be divided by 2"); //request user input
+            string numberInput = Console.ReadLine(); //collect user input
+            int userNumber = Convert.ToInt32(numberInput);
+        
+            DivisionOperation divisionOperation = new DivisionOperation();
+            DivisionOperation.userDivision(userNumber);
+            DivisionOperation.userDivision(userNumber);
+            //OperationHold operationHold = new OperationHold();
+            //OperationHold.UserOperation(userNumber, userNumber2);
+
+        }
+
+    }
+}
+
+
+
+
+
