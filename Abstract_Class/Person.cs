d69225183b41dd1ddf87d790c003a0c06cd80cd2
Mstewart93 <melyssaannestewart@gmail.com>
2024-09-23 06:51:42 -0600

    Abstract class
    Practiced Abstract Class using inheritance and class properties.

---
+++ Abstract_Class/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public abstract class Person //makes person abstract means that it can never be instantiated only inherited from 
+    {
+        public string FirstName; //propertie of class Person
+        public string LastName; // Propertie of class person
+        public void SayName() //method/function to print our the assigned values for FirstName and LastName
+        {
+
+            Console.WriteLine("Name:" + " " + FirstName + " " + LastName);
+        }
+
+    }
+}
---
+++ Abstract_Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using Abstract_Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Employee employee = new Employee(); //instantiate the new class
+            employee.FirstName = "Sample"; //assign a value to First Name, something Employee inherits from person
+            employee.LastName = "Student"; //same as above but last name
+            employee.SayName(); // calls teh function/method we created in person called sayname
+
+
+
+
+        }
+    }
+}
