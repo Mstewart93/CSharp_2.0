d8d49ce1c12011c05caea24f693111a6e60c4a8b
Mstewart93 <melyssaannestewart@gmail.com>
2024-09-28 09:20:39 -0600

    Polymorphism
    Practiced utilizing polymorphism with interfaces with c-sharp classes.

---
+++ Polymorphism_Submission_Assignent/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Submission_Assignent
+{
+    internal class Employee:Person, IQuittable
+    {
+        public void quit()
+        { 
+         Console.WriteLine(FirstName + " " + LastName + " " + " has quit school, and will not be returing");
+        
+        }
+       
+    }
+}
---
+++ Polymorphism_Submission_Assignent/Program.cs
@@ -0,0 +1,31 @@
+﻿//Create an interface called IQuittable and have it define a void method called Quit().
+
+//Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+
+//Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Submission_Assignent
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Employee employee = new Employee(); //instantiate the new class
+            employee.FirstName = "Sample"; //assign a value to First Name, something Employee inherits from person
+            employee.LastName = "Student"; //same as above but last name
+            employee.SayName(); // calls teh function/method we created in person called saynam
+            employee.quit();
+
+
+        }
+    }
+
+}
