88817932d25a597ee3f42e65b663fba0b48d0704
Mstewart93 <melyssaannestewart@gmail.com>
2024-11-10 13:13:22 -0700

    EnumChallange
    Working on the Challange for the Enum type.

---
+++ EnumChallange/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace Class_Submission_Assignment
+{
+    public class Program
+    {
+        public enum Days
+        {
+
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday,
+        }
+    public static void Main(string[] args)
+        {
+
+
+            try
+            {
+       
+                Console.WriteLine("Please Enter Today's Day of the Week:");
+               string UserDay = Console.ReadLine();
+                string UserLower = UserDay.ToLower();
+                if (Enum.IsDefined(typeof(Days), UserLower))
+                {
+                    Console.WriteLine("You entered " + UserLower);
+                }
+
+            }
+            
+            catch
+            {
+
+                Console.WriteLine("Please Enter an actual day of the week");
+                    }
+        }
+    }
+}
+         
+
+
+   
