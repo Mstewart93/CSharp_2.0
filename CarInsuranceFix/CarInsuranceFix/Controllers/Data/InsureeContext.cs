6e7a22fd9b077f482a62da5b42eda8d046c3a089
Mstewart93 <melyssaannestewart@gmail.com>
2024-12-31 08:31:14 -0700

    Working on car insuranse

---
+++ CarInsuranceFix/CarInsuranceFix/Controllers/Data/InsureeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarInsuranceFix.Models;
+
+namespace CarInsuranceFix.Controllers.Data
+{
+    public class InsureeContext : DbContext
+    {
+        public InsureeContext(DbContextOptions<InsureeContext> options) : base (options)
+        
+        { }
+        
+            
+        }
+    }
+
