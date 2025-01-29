896775e98d8bfe37f8a67704c03519d64d8b89c5
Mstewart93 <melyssaannestewart@gmail.com>
2025-01-29 15:56:31 -0700

    Software Bug trying to problem solvw

---
+++ CarInsurance/CarInsurance/Models/Insuree.cs
@@ -0,0 +1,25 @@
+﻿namespace CarInsurance.Models
+{
+    public class Insuree
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int DateOfBirth { get; set; }    
+        public Int32 CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool DUI { get; set; }
+        public Int32 SpeedingTickets { get; set; }
+        public string CoverageType { get; set; }
+
+        public decimal Quote {  get; set; }
+
+
+            
+           
+
+
+    }
+}
