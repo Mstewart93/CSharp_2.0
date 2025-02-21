0a46eb771c56d0f7978f27b2187eef31ef18d737
Mstewart93 <melyssaannestewart@gmail.com>
2025-02-21 10:00:50 -0700

    CodeFirst

---
+++ CodeFirstCoding/CodeFirstCoding/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstCoding
+{
+    public class SchoolContext: DbContext
+    {
+
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
