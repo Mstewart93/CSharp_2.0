e8bf5c6aba5a5d2b3402ff7d3c70fbea1580a2af
Mstewart93 <melyssaannestewart@gmail.com>
2025-02-21 09:46:04 -0700

    TimeApp

---
+++ TimeWebApp/TimeWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+namespace TimeWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public static DateTime Current { get; }
+
+    }
+}
+
+
